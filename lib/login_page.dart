dee5c592f5be104555a5fa0b519a7e2b9f306f57
admin123KK <karkiaku000@gmail.com>
2023-04-21 20:11:43 +0545

    inital commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -32,7 +32,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login Page'),
+        title: const Text('Login Pagee'),
       ),
       body: Container(
         padding: const EdgeInsets.all(10.0),
